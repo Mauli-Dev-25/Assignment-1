17994f5a03fc1dd858c7a57a5a5046dc7502b72c
unknown <chaudharidnyaneshwar160@gmail.com>
2026-02-25 17:35:29 +0530

    question1 ADDRD

---
+++ question_1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    
+    if (argc < 2) {
+        printf("Please provide numbers as command line arguments.\n");
+        return 1;
+    }
+
+    int max = atoi(argv[1]);
+
+    for (int i = 2; i < argc; i++) {
+        int num = atoi(argv[i]);
+        if (num > max) {
+            max = num;
+        }
+    }
+
+    printf("Maximum number is: %d\n", max);
+
+    return 0;
+}
