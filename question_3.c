14d19c713c82531ae068fd2648d95401524f0cd9
unknown <chaudharidnyaneshwar160@gmail.com>
2026-02-25 18:13:28 +0530

    3

---
+++ question_3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main(){
+    int i,n,k=1;
+    printf("Enter the Number");
+    scanf("%d",&n);
+
+    for(i=1;i<=n;i++);
+    {
+        k=k+i;
+
+    }
+    printf("%d",k);
+
+
+}
